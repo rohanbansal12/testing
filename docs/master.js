95be81824a6365ab30eb8d2d520b85db7783df80
rohanbansal12 <rohan2003bansal@gmail.com>
2021-08-10 02:38:35 -0500

    Update

--- docs/master.js
+++ docs/master.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $(this).attr("target", "_blank");
   });
 
-  $("#tab_metrics").click(function () {
+  $("#type_metric_refresh").click(function () {
     var current_selected_tab = $("section.is-active");
     current_selected_tab.find(".table100").remove();
     $("#metrics-spinner").show();
@@ -47,27 +47,20 @@ $(document).ready(function () {
         var ppu = item["last_sale"]["price"] / item["last_sale"]["weight"];
         var mc = item["total_weight"] * ppu;
         var current_html = "<tr>";
-        current_html += '<td class="column1">${item["crystal_type"]}</td>';
-        current_html += '<td class="column2">${item["rarity_percentage"]}</td>';
-        current_html +=
-          '<td class="column3">${item["last_sale"]["weight"]}</td>';
-        current_html +=
-          '<td class="column4">${item["last_sale"]["price"]}</td>';
-        current_html += '<td class="column5">${item["total_weight"]}</td>';
-        current_html += '<td class="column6">${ppu.toFixed(8)}</td>';
-        current_html += '<td class="column6">${mc.toFixed(1)}</td>';
+        current_html += `<td class="column1">${item["crystal_type"]}</td>`;
+        current_html += `<td class="column2">${item["rarity_percentage"]}</td>`;
+        current_html += `<td class="column3">${item["last_sale"]["weight"]}</td>`;
+        current_html += `<td class="column4">${item["last_sale"]["price"]}</td>`;
+        current_html += `<td class="column5">${item["total_weight"]}</td>`;
+        current_html += `<td class="column6">${ppu.toFixed(8)}</td>`;
+        current_html += `<td class="column6">${mc.toFixed(1)}</td>`;
         current_html += "</tr>";
         grand_html += current_html;
       });
       grand_html += "</tbody></table></div>";
+
       current_selected_tab.find(".spinner").hide();
       current_selected_tab.find(".wrap-table100").append(grand_html);
     })();
-  });
-
-  $(".toggle_words").click(function () {
-    console.log($(this).siblings(".top_words").text());
-    $(this).siblings(".top_words").toggle(500);
-    $(this).siblings(".least_words").toggle(500);
   });
 });
