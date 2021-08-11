0f5c09f85571f1623e2972e5d4f818f352e4aadb
rohanbansal12 <rohan2003bansal@gmail.com>
2021-08-11 03:01:15 -0500

    Update

---
+++ docs/js/hover.js
@@ -0,0 +1,101 @@
+$(document).ready(function () {
+  $("#fixed-tab-metrics")
+    .find(".column1")
+    .hover(
+      function () {
+        $(this)
+          .css("cursor", "pointer")
+          .attr("title", "The type/kind of crystal");
+      },
+      function () {
+        $(this).css("cursor", "auto");
+      }
+    );
+
+  $("#fixed-tab-metrics")
+    .find(".column2")
+    .hover(
+      function () {
+        $(this)
+          .css("cursor", "pointer")
+          .attr("title", "The real-world rarity of this crystal type");
+      },
+      function () {
+        $(this).css("cursor", "auto");
+      }
+    );
+
+  $("#fixed-tab-metrics")
+    .find(".column3")
+    .hover(
+      function () {
+        $(this)
+          .css("cursor", "pointer")
+          .attr("title", "The weight of the last sale of this crystal type");
+      },
+      function () {
+        $(this).css("cursor", "auto");
+      }
+    );
+
+  $("#fixed-tab-metrics")
+    .find(".column4")
+    .hover(
+      function () {
+        $(this)
+          .css("cursor", "pointer")
+          .attr(
+            "title",
+            "The price of the last sale of this crystal type (in Ξ)"
+          );
+      },
+      function () {
+        $(this).css("cursor", "auto");
+      }
+    );
+
+  $("#fixed-tab-metrics")
+    .find(".column5")
+    .hover(
+      function () {
+        $(this)
+          .css("cursor", "pointer")
+          .attr("title", "The total weight in existence of this crystal type");
+      },
+      function () {
+        $(this).css("cursor", "auto");
+      }
+    );
+
+  $("#fixed-tab-metrics")
+    .find(".column6")
+    .hover(
+      function () {
+        $(this)
+          .css("cursor", "pointer")
+          .attr(
+            "title",
+            "Price divided by the weight of the last sale of this crystal type"
+          );
+      },
+      function () {
+        $(this).css("cursor", "auto");
+      }
+    );
+
+  $("#fixed-tab-metrics")
+    .find(".column7")
+    .hover(
+      function () {
+        $(this)
+          .css("cursor", "pointer")
+          .attr(
+            "title",
+            "Unit price multiplied with the total weight of this crystal type"
+          );
+      },
+      function () {
+        $(this).css("cursor", "auto");
+      }
+    );
+});
